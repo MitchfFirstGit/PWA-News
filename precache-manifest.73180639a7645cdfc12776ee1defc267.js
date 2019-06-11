91de1a78491e828411bc8eccb056dcf9591504bf
Mitch <artfixmitch@gmail.com>
2019-06-11 23:27:28 +0300

    Updates

---
+++ precache-manifest.73180639a7645cdfc12776ee1defc267.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cb0c8de4cb75f2add21e7b1bae41465",
+    "url": "/PWA-News/index.html"
+  },
+  {
+    "revision": "929344728b2c6c0212b8",
+    "url": "/PWA-News/static/css/main.88e35543.chunk.css"
+  },
+  {
+    "revision": "65afd0f942bc561c8730",
+    "url": "/PWA-News/static/js/2.07f1278f.chunk.js"
+  },
+  {
+    "revision": "929344728b2c6c0212b8",
+    "url": "/PWA-News/static/js/main.7fec831b.chunk.js"
+  },
+  {
+    "revision": "de7bd840703825c4d91e",
+    "url": "/PWA-News/static/js/runtime~main.a21f9898.js"
+  }
+]);
